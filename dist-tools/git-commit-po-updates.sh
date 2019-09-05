6b70292d33eae6df4882704ebc406953b5c1b38f
Matthias Andree <matthias.andree@gmx.de>
2019-09-05 22:58:35 +0200

    Decode .po's author name to locale according to Content-Type header.

--- dist-tools/git-commit-po-updates.sh
+++ dist-tools/git-commit-po-updates.sh
@@ -31,27 +31,33 @@ done
 git diff -G ^.Project-Id-Version --name-only po/*.po \
 | while read pofile ; do 
 	if ! cmd="$(perl -WT - "$pofile" <<'_EOF'
+use Encode::Locale;
+use Encode;
 use strict;
 use Carp::Always ();
 use warnings FATAL => 'uninitialized';
-my ($ver, $dat, $trl, $lang, $lcod, $found);
+my ($ver, $dat, $trl, $lang, $lcod, $cset, $found);
 
 while(<>)
 {
-	if (/^"Project-Id-Version: (.+)\\n"/)	{ $ver=$1 };
-	if (/^"PO-Revision-Date: (.+)\\n"/)	{ $dat=$1 };
-	if (/^"Last-Translator: (.+)\\n"/)	{ $trl=$1 };
+	if (/^"Project-Id-Version: (.+)\\n"/)	{ $ver=$1; };
+	if (/^"PO-Revision-Date: (.+)\\n"/)	{ $dat=$1; };
+	if (/^"Last-Translator: (.+)\\n"/)	{ $trl=$1; };
 	if (/^"Language-Team: ([^<]+?)\s+<.*>\\n"/)
-						{ $lang=$1 };
-	if (/^"Language: (.+)\\n"/)		{ $lcod=$1 };
-	if ($ver and $dat and $trl and $lang and $lcod) {
+						{ $lang=$1; };
+	if (/^"Language: (.+)\\n"/)		{ $lcod=$1; };
+	if (/^"Content-Type: text\/plain; charset=(.+)\\n"/)
+						{ $cset = $1; };
+	if ($ver and $dat and $trl and $lang and $lcod and $cset) {
 		$found = 1;
 		last; 
 	}
 }
 
+$trl = Encode::decode($cset, $trl);
+
 if ($found) {
-	print "git commit --author '$trl' --date '$dat' -m 'Update <$lcod> $lang translation to $ver'"
+	print Encode::encode(locale => "git commit --author '$trl' --date '$dat' -m 'Update <$lcod> $lang translation to $ver'", Encode::FB_CROAK);
 } else {
 	exit(1);
 }
