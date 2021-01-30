58cd80026cce0c2ff1daa5eb818f34c6db1622e6
Matthias Andree <matthias.andree@gmx.de>
2021-01-30 17:30:46 +0100

    tls-aux.h: Remove unneeded 1.0.2 compatibility code.

--- tls-aux.h
+++ tls-aux.h
@@ -8,11 +8,6 @@
 #ifdef SSL_ENABLE
 #include <openssl/opensslv.h>
 
-# if defined(LIBRESSL_VERSION_NUMBER) || OPENSSL_VERSION_NUMBER < 0x1010000fL
-#  undef OSSL110_API
-# else
-#  define OSSL110_API 1
-# endif
 # if OPENSSL_VERSION_NUMBER < 0x1010000fL
 #  define OpenSSL_version(t) SSLeay_version((t))
 #  define OpenSSL_version_num() SSLeay()
